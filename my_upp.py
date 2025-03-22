d9c9481683eb41f062a81b3e1d93db37200dcb71
shrekxy999 <furrypinkcloud@gmail.com>
2025-03-22 18:32:43 +0300

    Update my_upp.py

--- my_upp.py
+++ my_upp.py
@@ -1,3 +1,4 @@
+from instr import *
 class MainWin(QWidget):
   def __init__(self):
     super().__init__()
@@ -13,6 +14,21 @@ class MainWin(QWidget):
     def connects(self): 
         pass
   def set_appear(self):
+    
     self.setWindowTitle(txt_title)
     self.resize(win_width, win_height)
     self.move(win_x, win_y)
+    def initUI(self):
+self.hello_text = QLabel( txt_hello )
+self. instruction = QLabel( txt_instruction)
+self. button = PushButton( txt_next )
+self.layout = QVBoxLayout()
+self.layout .addWidget(self.hello_text)
+self. layout.addWidget(self instruction)
+self. layout.addWidget(self.button)
+
+from instr import * class MainWin(QWidget) :
+def initUI(self):
+self.hello_text = Label(txt_hello)
+self. instruction = QLabel(txt_instruction)
+self.button = PushButton(txt_next)
